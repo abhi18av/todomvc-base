8121a1d9783efb3fb116bfd72f3583cc1cbc95b2
Abhinav Sharma <abhi18av@gmail.com>
2019-11-25 19:55:50 +0530

    identify exactly which element is clicked

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import React from "react";
 // App
 //=============================
 
-
 export class App extends React.Component {
     constructor() {
         super();
@@ -20,68 +19,74 @@ export class App extends React.Component {
             todos: [],
             todo: null
         };
-    };
+    }
 
-    updateInput = (e) => {
+    updateInput = e => {
         this.setState({todo: e.target.value});
     };
 
     // NOTE it's good to have this as a separate method but now it's almost indistinguishable from the event handlers
     // TODO merge this function with the submitTodo
-    addTodo = async (todo) => {
-        await this.setState({todos: [...this.state.todos, {text: todo, completed: false}]});
+    addTodo = async todo => {
+        await this.setState({
+            todos: [...this.state.todos, {text: todo, completed: false}]
+        });
 
-        localStorage.setItem('todos', JSON.stringify(this.state.todos));
+        localStorage.setItem("todos", JSON.stringify(this.state.todos));
 
-        document.getElementById('inputBox').value = '';
+        document.getElementById("inputBox").value = "";
         console.log(this.state.todo);
     };
 
-    submitTodo = (e) => {
+    submitTodo = e => {
         e.preventDefault();
         this.addTodo(this.state.todo);
     };
 
-
-    updateTodo = (todo) => {
+    updateTodo = todo => {
         const newTodos = this.state.todos.map(_todo => {
-            if (todo === _todo) {
-                return {
+            if (_todo === todo) {
+                const aTodo = {
                     text: _todo.text,
                     completed: !_todo.completed
-                }
+                };
+                console.log(aTodo);
+                return aTodo;
             } else {
                 return _todo;
             }
         });
-    };
 
+        console.log("newTodos: ", newTodos);
+    };
 
     toggleTodo = () => {
         this.updateTodo(this.state.todo);
-        console.log('toggleTodo');
-        console.log(this.state.todos);
     };
 
-    componentDidMount() {
+    // DONE detect which todo is being clicked
+    // clickDetecter = e => {
+    //     console.log(e.target.innerText);
+    //     const clickedTodo = e.target.innerText;
+    //     console.log(this.state.todos.filter(_todo => {
+    //         return _todo.text === clickedTodo
+    //     }));
+    // };
 
-        const todos = localStorage.getItem('todos');
+    componentDidMount() {
+        const todos = localStorage.getItem("todos");
 
         if (todos) {
             const savedTodos = JSON.parse(todos);
             console.log("HAS todos", savedTodos);
             this.setState({todos: savedTodos});
-
         } else {
             console.log("NO todos");
         }
-    };
-
+    }
 
     render() {
-
         const {todos} = this.state;
-
 
         return (
             <>
@@ -89,29 +94,25 @@ export class App extends React.Component {
                     <header className="App-header">Master React</header>
                 </div>
 
-
-                <form onSubmit={(e) => this.submitTodo(e)}>
+                <form onSubmit={e => this.submitTodo(e)}>
                     <input
-                        id='inputBox'
-                        type='text'
-                        onChange={(e) => this.updateInput(e)}>
-                    </input>
+                        id="inputBox"
+                        type="text"
+                        onChange={e => this.updateInput(e)}
+                    ></input>
 
-                    <button
-                        type='submit'>
-                        Add Task
-                    </button>
+                    <button type="submit">Add Task</button>
                 </form>
 
-
-                { // render the current tasks of `todos` variable
-                    todos.map((_aTask, _index) => <div
-                        key={_index}
-                        onClick={this.toggleTodo}>
-                        {_aTask.text}
-                    </div>)
-                }
-
+                {// render the current tasks of `todos` variable
+                    todos.map((_aTask, _index) => (
+                        <div key={_index}
+                            // onClick={e => this.clickDetecter(e)}
+                             onClick={e => this.clickDetecter(e)}
+                        >
+                            {_aTask.text}
+                        </div>
+                    ))}
             </>
         );
     }
