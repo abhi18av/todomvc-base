009396d3f6b60738c007260eecfd8e497c7947d0
Abhinav Sharma <abhi18av@gmail.com>
2019-11-25 18:00:55 +0530

    working on reading the user input

--- src/App.js
+++ src/App.js
@@ -15,16 +15,36 @@ import React from "react";
 
 export class App extends React.Component {
     constructor() {
-        super(props);
-        this.state = [];
+        super();
+        this.state = {todos: []};
     }
 
+
+    onSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log(e);
+    };
+
+
     render() {
+
+        const {todos} = this.state;
+
+
         return (
             <>
                 <div className="App">
                     <header className="App-header">Master React</header>
                 </div>
+
+
+                <form onSubmit={(e) => this.onSubmitHandler(e)}>
+                    <input
+                        type='text'>
+                    </input>
+                    <button type='button'>Add Task</button>
+                </form>
+
             </>
         );
     }
